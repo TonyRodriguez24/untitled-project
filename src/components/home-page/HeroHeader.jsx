be4c6e8f16bafc02ef722faf9ee51bd92922e216
Tony <tonyrodriguez26@outlook.com>
2025-07-06 16:20:36 -0400

    fixing hero header thats causing weird tear in mobile view

--- src/components/home-page/HeroHeader.jsx
+++ src/components/home-page/HeroHeader.jsx
@@ -15,7 +15,7 @@ export default function HeroHeader({
     <div className="w-full flex justify-center sm:px-8 lg:px-10">
       <div className="grid grid-cols-1 lg:grid-cols-12 lg:gap-10 max-w-screen-2xl w-full items-stretch lg:py-10">
         {/* Text Section */}
-        <div className="p-3 lg:p-0 col-span-12 lg:col-span-6 flex flex-col justify-center gap-4 lg:gap-6 text-white h-[calc(100dvh-64px)] lg:h-auto">
+        <div className="p-3 lg:p-0 col-span-12 lg:col-span-6 flex flex-col justify-center gap-4 lg:gap-6 text-white h-[calc(var(--vh,1vh)*100-64px)] lg:h-auto">
           <h1 className="text-5xl font-extrabold select-none">{title}</h1>
           <p className="text-lg lg:text-xl select-none">{subtitle}</p>
           <Button CTA={buttonText} href={buttonHref} />
